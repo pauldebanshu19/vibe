9ebfeef15beccf2be00faf514ef14df20ae107b4
Debanshu Paul <pauldebanshu73@gmail.com>
2025-07-11 23:25:20 +0530

    feat: add @e2b/code-interpreter dependency and integrate sandbox functionality in functions

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,18 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { gemini, createAgent } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
+import { getSandbox } from "./util";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-development-2");
+      return sandbox.sandboxId;
+    });  
+
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert nextjs developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
@@ -15,6 +22,12 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Write the following snippet: ${event.data.value}`,
     );
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    })  
 
     return { output };
   },
